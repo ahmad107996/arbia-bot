539ceb2c64fb102c61e0ba247c652644dbbeeec8
ahmad107996 <36535354+ahmad107996@users.noreply.github.com>
2019-03-09 21:34:57 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -23,6 +23,20 @@ if (!channel) return;
 channel.send({embed : embed});
 });
 
+client.on('message', message => {  
+    if (message.author.bot) return;
+if (message.content.startsWith(prefix + 'clear')) { //Codes
+    if(!message.channel.guild) return message.reply('⛔ | This Command For Servers Only!'); 
+        if(!message.member.hasPermission('MANAGE_MESSAGES')) return message.channel.send('⛔ | You dont have **MANAGE_MESSAGES** Permission!');
+        if(!message.guild.member(client.user).hasPermission('MANAGE_MESSAGES')) return message.channel.send('⛔ | I dont have **MANAGE_MESSAGES** Permission!');
+ let args = message.content.split(" ").slice(1)
+    let messagecount = parseInt(args);
+    if (args > 99) return message.reply("**🛑 || يجب ان يكون عدد المسح أقل من 100 .**").then(messages => messages.delete(5000))
+    if(!messagecount) args = '100';
+    message.channel.fetchMessages({limit: messagecount + 1}).then(messages => message.channel.bulkDelete(messages));
+    message.channel.send(`\`${args}\` : __عدد الرسائل التي تم مسحها __ `).then(messages => messages.delete(5000));
+  }
+  }); //Julian
 
 client.on('guildMemberRemove', member => {
 let channel = member.guild.channels.find('name', 'welcome');
@@ -408,6 +422,9 @@ client.on('message', message => {
 
   **${prefix}c.t**
   عشان يسوي لك الرومات الرئسيه
+
+  **${prefix}clear**
+     لمسح الرسايل
 
   **${prefix}mute**
 مـيوت بدون رتبة
